bde1b990f07c29b19f45bae570bba163987f5af3
Kadellopez <kadellopez@outlook.com>
2024-05-21 17:36:30 -0400

    exercises 7 to 12

--- app.js
+++ app.js
@@ -69,4 +69,96 @@ Solve Exercise 6 here:
 //This one took me a while to research how to target specific key
 //elements within the object array. I was looking for the specific
 //modifier that targeted .hp within the function everything else was straight forward.
+//What I used to solve the issue => https://www.w3schools.com/js/js_array_sort.asp
 game.party.sort(function(a,b){return a.hp - b.hp}).reverse();
+
+/*
+Exercise 7
+1. Set the `completed` property to true for gyms with a difficulty below 3.
+2. Think about how you'd loop through the `gyms` array to check and update the `completed` property.
+
+
+Solve Exercise 7 here:
+*/
+game.gyms.forEach((gyms) => {
+    if (gyms.difficulty < 3) {
+        gyms.completed = true;
+    }
+});
+
+/*
+Exercise 8
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. Remember that you're working with an array of objects - what array method is ideal for replacing one element with another? 
+
+Solve Exercise 8 here:
+*/
+game.party[2] = pokemon[7]
+game.party.sort(function(a,b){return a.hp - b.hp}).reverse();
+
+/*
+Exercise 9
+1. Print the name of each Pokémon in your party.
+2. Consider using a loop or an array method to access each Pokémon's name.
+
+Solve Exercise 9 here:
+*/
+game.party.forEach((party) => {
+      console.log(party.name);
+});
+
+/*
+Exercise 10
+1. Can you print out all the starter Pokémon from the `pokemon` array?
+2. Think about how you can identify a starter Pokémon and then log their names.
+
+
+Solve Exercise 10 here:
+*/
+pokemon.forEach((poke) => {
+   if (poke.starter && true) {
+    console.log(poke);
+   }
+});
+
+/*
+Exercise 11
+1. Add a method called `catchPokemon` to the `game` object. This method should:
+  - Accept an object as a parameter called `pokemonObj`
+  - Add the `pokemonObj` to the `game.party` array.
+  - not return anything
+
+After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+Solve Exercise 11 here:
+*/
+game.catchPokemon = (pokemonObj) => {
+    game.party.push(pokemonObj);
+}
+
+game.catchPokemon(pokemon[101])
+
+/*
+Exercise 12
+1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+Tips:
+For this exercise, it's okay to have a negative number of pokeballs.
+After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 12 here:
+*/
+//this is as far as i could get
+     
+
+
